7f66438295996a61474b9d995f3c35d198e04c72
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-02-24 21:23:27 +0300

    index.ts

--- ●	src/practice/classes/index.ts
+++ ●	src/practice/classes/index.ts
@@ -1,6 +1,30 @@
 import { Counter } from "./Counter";
+
 const counter = new Counter();
 console.log("Экземпляр Counter:", counter);
 
+
 console.log("Начальное значение value:", counter.value); // 0
+
+counter.increment();
+counter.increment();
+console.log("После двух increment:", counter.value); // 2
+
+counter.decrement();
+console.log("После decrement:", counter.value); // 1
+
+counter.reset();
+console.log("После reset:", counter.value); // 0
+
+
+const a = new Counter();
+const b = new Counter();
+
+a.increment();
+a.increment();
+
+b.increment(); 
+
+console.log("a.value =", a.value);
+console.log("b.value =", b.value);
 
